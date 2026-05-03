dfd543c8f2dca39e46491d57c0e77dfd7d3690c4
Miqueas Correa <miki.mate.tizi@gmail.com>
2026-05-03 10:48:38 -0300

    fix: eliminar importación y uso de compress_dynamic_range en audio_service

--- src/service/audio_service.py
+++ src/service/audio_service.py
@@ -1,6 +1,6 @@
 import io
 from pydub import AudioSegment
-from pydub.effects import normalize, compress_dynamic_range
+from pydub.effects import normalize
 
 def quality_audio(file) -> bytes:
     extension = file.filename.rsplit(".", 1)[1].lower()
@@ -14,9 +14,6 @@ def quality_audio(file) -> bytes:
 
     # 3. Normalizar volumen
     audio = normalize(audio, headroom=0.5)
-
-    # 4. Comprimir rango dinámico levemente
-    audio = compress_dynamic_range(audio, threshold=-20.0, ratio=2.0, attack=5.0, release=50.0)
 
     buffer = io.BytesIO()
     audio.export(buffer, format="wav")
